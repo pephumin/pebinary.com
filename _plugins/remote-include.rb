b2fbceaf1e7f8fcdacaff7d2c23615396703368a
Phumin Chesdmethee <phumin@sawasdee.org>
2016-07-08 20:21:11 +0700

    Some cosmetic changed
    Some cosmetic changed

---
+++ _plugins/remote-include.rb
@@ -0,0 +1,27 @@
+# remote include files by using below command
+# {% remote_include http://static.2inspire.co.uk/img/vector.svg %}
+
+require 'net/http'
+require 'uri'
+
+module Jekyll
+
+  class RemoteInclude < Liquid::Tag
+
+    def initialize(tag_name, remote_include, tokens)
+      super
+      @remote_include = remote_include
+    end
+
+    def open(url)
+      Net::HTTP.get(URI.parse(url)).force_encoding 'utf-8'
+    end
+
+    def render(context)
+      open("#{@remote_include}")
+    end
+
+  end
+end
+
+Liquid::Template.register_tag('remote_include', Jekyll::RemoteInclude)
