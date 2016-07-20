ae817e47d98452d65430948011185a4206297174
Phumin Chesdmethee <phumin@sawasdee.org>
2016-07-20 20:21:03 +0700

    Remove unused files
    Remove unused files

--- _x/_plugin/geo_pattern/errors.rb
+++
@@ -1,7 +0,0 @@
-module GeoPattern
-  # user errors
-  class UserError < StandardError; end
-
-  # raised if an invalid pattern is requested
-  class InvalidPatternError < UserError; end
-end
--- _x/_plugin/geo_pattern/version.rb
+++
@@ -1,3 +0,0 @@
-module GeoPattern
-  VERSION = '1.4.0'
-end
