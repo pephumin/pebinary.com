3ae074a24b5d4107cfac730ed70ce54732c43a6e
Phumin Chesdmethee <phumin@sawasdee.org>
2016-07-21 08:53:05 +0700

    Make the main topnav clickable
    Make the main topnav clickable

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -14,6 +14,19 @@ $(document).ready(function() {
   });
 });
 
+// $(function($) {
+//     if($(window).width()>769){
+//         $('.navbar .dropdown').hover(function() {
+//             $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+//         }, function() {
+//             $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+//         });
+//         $('.navbar .dropdown > a').click(function(){
+//             location.href = this.href;
+//         });
+//     }
+// });
+
 // contact form (lifted up version)
 
 $(function(){
