921d205701ae75bda4fe4b17c7a843382e2f5050
Phumin Chesdmethee <phumin@sawasdee.org>
2016-07-31 01:13:56 +0700

    Add "view demo" section
    Add "view demo" section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,17 +1,17 @@
 // sticky top nav
 
 $(document).ready(function() {
-  var navOffset = $("nav").offset().top;
-  $(".navbar").wrap('<div class="nav-placeholder"></div>');
-  $(".nav-placeholder").height($(".navbar").outerHeight());
-  $(window).scroll(function() {
-      var scrollPos = $(window).scrollTop();
-      if (scrollPos >= navOffset) {
-        $(".navbar").addClass("fixed");
-      } else {
-        $(".navbar").removeClass("fixed");
-      }
-  });
+    var navOffset = $("nav").offset().top;
+    $(".navbar").wrap('<div class="nav-placeholder"></div>');
+    $(".nav-placeholder").height($(".navbar").outerHeight());
+    $(window).scroll(function() {
+        var scrollPos = $(window).scrollTop();
+        if (scrollPos >= navOffset) {
+            $(".navbar").addClass("fixed");
+        } else {
+            $(".navbar").removeClass("fixed");
+        }
+    });
 });
 
 // $(function($) {
@@ -29,26 +29,31 @@ $(document).ready(function() {
 
 // contact form (lifted up version)
 
-$(function(){
-  $('.contact-form .input-group input').focusout(function(){
-    var text_val = $(this).val();
-    if(text_val === "") {
-      $(this).removeClass('has-value');
-    } else {
-      $(this).addClass('has-value');
-    }
-  });
+$(function() {
+    $('.contact-form .input-group input').focusout(function() {
+        var text_val = $(this).val();
+        if (text_val === "") {
+            $(this).removeClass('has-value');
+        } else {
+            $(this).addClass('has-value');
+        }
+    });
 });
 
 // back to top
 
 $(document).ready(function() {
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) { $(".scroll-to-top").fadeIn() }
-        else { $(".scroll-to-top").fadeOut() }
+        if ($(this).scrollTop() > 100) {
+            $(".scroll-to-top").fadeIn()
+        } else {
+            $(".scroll-to-top").fadeOut()
+        }
     });
     $(".scroll-to-top").click(function() {
-        $("html, body").animate({ scrollTop: 0 }, 800);
+        $("html, body").animate({
+            scrollTop: 0
+        }, 800);
         return false
     })
 });
@@ -56,7 +61,7 @@ $(document).ready(function() {
 // anchor link
 
 anchors.options = {
-  placement: 'right',
-  visible: 'hover'
+    placement: 'right',
+    visible: 'hover'
 };
 anchors.add('h2, h3, h4');
